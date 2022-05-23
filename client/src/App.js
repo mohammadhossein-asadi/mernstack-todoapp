7ef57545b5b7d1a88fab84a38112d10f4d3a3ace
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-05-23 23:36:05 +0430

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,26 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <form>
+      <h1>Todo List</h1>
+      <form className="form">
         <input type="text" placeholder="Add Todo Item" />
         <button type="submit">Add</button>
       </form>
       <div className="todo-listItems">
         <div className="todo-item">
-          <p>This is the item 1</p>
-          <button>Update</button>
-          <button>Delete</button>
+          <p className="item-content">This is the item 1</p>
+          <button className="update-item">Update</button>
+          <button className="delete-item">Delete</button>
         </div>
         <div className="todo-item">
-          <p>This is the item 2</p>
-          <button>Update</button>
-          <button>Delete</button>
+          <p className="item-content">This is the item 2</p>
+          <button className="update-item">Update</button>
+          <button className="delete-item">Delete</button>
         </div>
         <div className="todo-item">
-          <p>This is the item 3</p>
-          <button>Update</button>
-          <button>Delete</button>
+          <p className="item-content">This is the item 3</p>
+          <button className="update-item">Update</button>
+          <button className="delete-item">Delete</button>
         </div>
       </div>
     </div>
