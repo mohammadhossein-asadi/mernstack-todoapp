8036118963b2873495ba3d99c6e00d135fccd0ee
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-05-23 22:50:09 +0430

    commit

--- server/index.js
+++ server/index.js
@@ -8,9 +8,13 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5500;
 
+const TodoItemRoute = require("./routes/todoItems");
+
 mongoose
   .connect(process.env.DB_CONNECT)
   .then(() => console.log("Database connected"))
   .catch((err) => console.log(err));
+
+app.use("/", TodoItemRoute);
 
 app.listen(PORT, () => console.log("Server connected"));
--- server/routes/todoItems.js
+++ server/routes/todoItems.js
@@ -1,3 +1,37 @@
 const router = require("express").Router();
 
 const todoItemsModel = require("../models/todoItems");
+
+router.post("/api/item", async (req, res) => {
+  try {
+    const newItem = new todoItemsModel({
+      item: req.body.item,
+    });
+    const saveItem = await newItem.save();
+    res.status(200).json("Item Added Successfully.");
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+router.get("/api/items", async (req, res) => {
+  try {
+    const allTodoItems = await todoItemsModel.find({});
+    res.status(200).json(allTodoItems);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.put("/api/item/:id", async (req, res) => {
+  try {
+    const updateItem = todoItemsModel.findByIdAndUpdate(req.params.id, {
+      $set: req.body,
+    });
+    res.status(200).json("Item Updated");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+module.exports = router;
