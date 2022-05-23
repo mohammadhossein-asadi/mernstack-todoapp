7d5dd6a61c97e1f6d09d5032dfbeb7f4e7fa5308
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-05-24 00:17:57 +0430

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
 function App() {
   const [itemText, setItemText] = useState("");
+  const [listItems, setListItems] = useState([]);
 
   const addItem = async (e) => {
     e.preventDefault();
@@ -11,6 +12,28 @@ function App() {
       const res = await axios.post("http://localhost:5500/api/item", {
         item: itemText,
       });
+      console.log(res);
+      setItemText("");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    const getItemsList = async () => {
+      try {
+        const res = await axios.get("http://localhost:5500/api/items");
+        setListItems(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getItemsList();
+  }, []);
+
+  const deleteItem = async (id) => {
+    try {
+      const res = await axios.delete(`http://localhost:5500/api/item/${id}`);
       console.log(res);
     } catch (err) {
       console.log(err);
@@ -32,21 +55,13 @@ function App() {
         <button type="submit">Add</button>
       </form>
       <div className="todo-listItems">
-        <div className="todo-item">
-          <p className="item-content">This is the item 1</p>
-          <button className="update-item">Update</button>
-          <button className="delete-item">Delete</button>
-        </div>
-        <div className="todo-item">
-          <p className="item-content">This is the item 2</p>
-          <button className="update-item">Update</button>
-          <button className="delete-item">Delete</button>
-        </div>
-        <div className="todo-item">
-          <p className="item-content">This is the item 3</p>
-          <button className="update-item">Update</button>
-          <button className="delete-item">Delete</button>
-        </div>
+        {listItems.map((item) => (
+          <div className="todo-item">
+            <p className="item-content">{item.item}</p>
+            <button className="update-item">Update</button>
+            <button className="delete-item">Delete</button>
+          </div>
+        ))}
       </div>
     </div>
   );
