ccebf231ee3ef5553c2bfb25dd94d267e0d8764f
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-05-23 22:03:41 +0430

    COMMIT

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5500;
+
+mongoose
+  .connect(process.env.DB_CONNECT)
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.log(err));
+
+app.listen(PORT, () => console.log("Server connected"));
